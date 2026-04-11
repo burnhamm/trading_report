fa2cfd1a559f746e1469c1c1efc8d0c9e3b41a78
Yurii Nevirkovets <nevy@viessmann.com>
2026-04-11 16:22:58 +0200

    format positions view

--- src/views/positions_view.py
+++ src/views/positions_view.py
@@ -3,7 +3,7 @@ import csv
 from decimal import Decimal
 from pyxirr import xirr
 
-from decimal_utils import normalize_decimal as nd
+from decimal_utils import normalize_money as nm
 from model.position import Position
 
 def generate_positions_view(positions: list[Position], fx_rate_provider, output_path: str):
@@ -20,14 +20,14 @@ def generate_positions_view(positions: list[Position], fx_rate_provider, output_
             yearly_profit = total_profit / length_days * 365 if length_days > 0 else 0
             yearly_profit_percent = yearly_profit / total_buy if total_buy > Decimal("0") else 0
             xirr_result = calculate_xirr(p, ex_rate)
-            data.append(["CLOSED", p.symbol, nd(p.quantity), p.currency,  p.open_date.date(), nd(p.buy_price), nd(total_buy), p.close_date.date(), nd(p.sell_price), nd(total_sell), nd(dividends), nd(taxes), nd(total_profit), length_days, f"{nd(yearly_profit_percent * 100)}%", nd(xirr_result)])
+            data.append(["CLOSED", p.symbol, p.quantity, p.currency,  p.open_date.date(), p.buy_price, nm(total_buy), p.close_date.date(), p.sell_price, nm(total_sell), nm(dividends), nm(taxes), nm(total_profit), length_days, nm(yearly_profit_percent), nm(xirr_result)])
         else:
-            data.append(["", p.symbol, nd(p.quantity), p.currency,  p.open_date.date(), nd(p.buy_price), nd(total_buy), "", "", "", nd(dividends), nd(taxes), "", "", "", ""])
+            data.append(["", p.symbol, p.quantity, p.currency,  p.open_date.date(), p.buy_price, nm(total_buy), "", "", "", nm(dividends), nm(taxes), "", "", "", ""])
 
     data.sort(key=lambda r: (r[1], r[3], r[4]))
     data.sort(key=lambda r: r[0], reverse=True)
     data.insert(0, 
-        ["Closed", "Ticker", "Amount", "Currency", "Buy date", "Buy price", "Total buy", "Sell date", "Sell price", "Total sell", "Dividends", "Taxes", "Net profit", "Position length (days)", "Yearly profit %, XIRR"])
+        ["Closed", "Ticker", "Amount", "Currency", "Buy date", "Buy price", "Total buy", "Sell date", "Sell price", "Total sell", "Dividends", "Taxes", "Net profit", "Position length (days)", "Annual profit", "XIRR"])
 
     with open(output_path + "/positions.csv", "w", newline='') as file:
         writer = csv.writer(file)

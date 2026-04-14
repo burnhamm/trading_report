0c1b62452d862a8fb2eeb88aa421e13c8c2b5181
Yurii Nevirkovets <nevy@viessmann.com>
2026-04-14 20:32:54 +0200

    positions view: rearrange columns and standardize names

--- src/views/positions_view.py
+++ src/views/positions_view.py
@@ -16,22 +16,26 @@ def generate_positions_view(positions: list[Position], fx_rate_provider, output_
         if pos.closed:
             proceeds = pos.quantity * pos.sell_price * ex_rate
             profit = proceeds - cost + dividends
-            length_days = max(1, (pos.close_date - pos.open_date).days)
-            duration = Decimal(length_days / 365.24)
+            duration_days = max(1, (pos.close_date - pos.open_date).days)
+            duration = Decimal(duration_days / 365.24)
             annualized = profit / duration / cost
             exposure = duration * cost
-            data.append(["CLOSED", pos.symbol, pos.quantity, pos.currency,  pos.open_date.date(), pos.buy_price, nm(cost), 
-                         pos.close_date.date(), pos.sell_price, nm(proceeds), nm(dividends), nm(taxes), nm(profit), length_days, 
-                         nm(annualized), nm(exposure)])
+            data.append(["CLOSED", pos.symbol, pos.currency, pos.quantity, nm(exposure),
+                         pos.open_date.date(), pos.buy_price, nm(cost), 
+                         pos.close_date.date(), pos.sell_price, nm(proceeds),
+                         nm(dividends), nm(taxes), nm(profit), duration_days, nm(annualized)])
         else:
-            data.append(["", pos.symbol, pos.quantity, pos.currency, pos.open_date.date(), pos.buy_price, nm(cost), "", "", "", 
-                         nm(dividends), nm(taxes), "", "", "", ""])
+            data.append(["", pos.symbol, pos.currency, "", pos.quantity, pos.open_date.date(), pos.buy_price, nm(cost), "", "", "", 
+                         nm(dividends), nm(taxes), "", "", ""])
 
-    data.sort(key=lambda r: (r[1], r[3], r[4]))
+    data.sort(key=lambda r: (r[1], r[2], r[5]))
     data.sort(key=lambda r: r[0], reverse=True)
     data.insert(0, 
-        ["Closed", "Ticker", "Amount", "Currency", "Buy date", "Buy price", "Total buy", "Sell date", "Sell price", "Total sell", 
-         "Dividends", "Taxes", "Net profit", "Position length (days)", "Annual profit", "Exposure"])
+        ["Closed", "Ticker", "Currency", "Quantity", f"Capital Exposure ({fx_rate_provider.base_currency} * years)", 
+         "Open date", f"Entry price ({fx_rate_provider.base_currency})", f"Cost Basis ({fx_rate_provider.base_currency})", 
+         "Close date", f"Exit price ({fx_rate_provider.base_currency})", f"Proceeds ({fx_rate_provider.base_currency})",
+         f"Dividends ({fx_rate_provider.base_currency})", f"Taxes ({fx_rate_provider.base_currency})",
+         f"Net Profit/Loss ({fx_rate_provider.base_currency})", "Holding period (days)", "Annual Return"])
 
     with open(output_path + "/positions.csv", "w", newline='') as file:
         writer = csv.writer(file)
